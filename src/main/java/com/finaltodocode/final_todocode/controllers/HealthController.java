37eada02c1b4d0ae62081df6c2eb1268d65675e2
DanielDeveloper19 <daniel256531@gmail.com>
2026-05-08 19:07:55 -0500

    Health check controller

---
+++ src/main/java/com/finaltodocode/final_todocode/controllers/HealthController.java
@@ -0,0 +1,20 @@
+package com.finaltodocode.final_todocode.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1") // Matches your Ingress path
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String> healthCheck() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("message", "Alive Systems Backend is reachable");
+        response.put("system_time", String.valueOf(System.currentTimeMillis()));
+        return response;
+    }
+}
